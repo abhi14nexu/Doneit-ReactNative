03adbe3e9ce9c247ea124bfe6dcee4ae9bfc97c0
Abhishek Yadav <abhishek.yadav14112000@gmail.com>
2020-08-01 23:45:11 +0530

    old repository deleted ...new one

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+import {useDimensions,useDeviceOrientation} from '@react-native-community/hooks';
 
 export default function App() {
+  // useDimensions.console.log();
+  console.log(useDimensions());
+  console.log(useDeviceOrientation());
+  const {landscape} =useDeviceOrientation();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // <View style={{height: landscape ?"100%":"30%",width:"100%",backgroundColor:'dodgerblue'}}>
+    //   <Text>this is a text</Text>
+    // </View>
+ 
+    <View style={{backgroundColor: "#fff",flex:1,flexDirection:'row', // horizontal
+    justifyContent:'center', //main
+    alignItems:'center' //column
+    }}>
+    <View style={{backgroundColor: "gold",width:100,height:100}} />
+    <View style={{backgroundColor: "cyan",width:100,height:100}} />
+    <View style={{backgroundColor: "tomato",width:100,height:100}} />
+    <View style={{backgroundColor: "dodgerblue",width:100,height:100}} />
+    
     </View>
+    
+    
   );
 }
 
